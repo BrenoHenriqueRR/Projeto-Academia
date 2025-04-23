2a65298d9408d1e1dc1e20675c7c77786d76067e
BrenoHenriqueRR <breno.reis@aluno.ifsp.edu.br>
2025-04-23 08:15:33 -0300

    carrinho na loja add

--- Front-end/src/app/components/admin/painel-admin/pn-loja/modal-venda/modal-venda.component.ts
+++ Front-end/src/app/components/admin/painel-admin/pn-loja/modal-venda/modal-venda.component.ts
@@ -1,26 +1,33 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select'
+import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatOptionModule } from '@angular/material/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-modal-venda',
   standalone: true,
   imports: [MatDialogModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
-    FormsModule,
-    BrowserAnimationsModule],
+      MatButtonModule,
+      MatFormFieldModule,
+      MatInputModule,
+      MatSelectModule,
+      MatOptionModule, 
+      FormsModule,CommonModule],
   templateUrl: './modal-venda.component.html',
   styleUrl: './modal-venda.component.css'
 })
 export class ModalVendaComponent {
   quantidade = 1;
   formaPagamento = 'dinheiro';
+
+  ngOnInit(){
+    console.log(this.data);
+  }
 
   constructor(
     public dialogRef: MatDialogRef<ModalVendaComponent>,
@@ -34,6 +41,7 @@ export class ModalVendaComponent {
       valor_total: this.data.produto.preco * this.quantidade,
       forma_pagamento: this.formaPagamento
     };
+    console.log(venda);
 
 
     this.dialogRef.close(true);

9c0ad4a2caf1c19e21364a6b80d9d48c6f72b42a
BrenoHenriqueRR <breno.reis@aluno.ifsp.edu.br>
2025-05-16 08:34:37 -0300

    ..

--- Front-end/src/app/components/admin/painel-admin/pn-treino/pn-treino.component.ts
+++ Front-end/src/app/components/admin/painel-admin/pn-treino/pn-treino.component.ts
@@ -2,52 +2,60 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CadTreinoService } from '../../../../services/cad-treino/cad-treino.service';
 import { CommonModule } from '@angular/common';
+import { PnClienteService } from '../../../../services/admin/pn-cliente/pn-cliente.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-pn-treino',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule,CommonModule],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule, ReactiveFormsModule,
+    MatFormFieldModule, MatInputModule, MatAutocompleteModule],
   templateUrl: './pn-treino.component.html',
   styleUrl: './pn-treino.component.css'
 })
 export class PnTreinoComponent {
+  clienteControl = new FormControl('');
   exer!: FormGroup;
   Grupo!: FormGroup;
   fichas!: any;
   clientes: any;
+  clientesFiltrados: any[] = [];
+  clienteSelecionado: any = null;
+  
+  constructor(private service: CadTreinoService, private cliservice: PnClienteService) { }
 
-  constructor(private service: CadTreinoService){
-    this.exer = new FormGroup({
-      exercicio: new FormControl(''),
+  ngOnInit() {
+    this.pesquisarCli();
+
+    this.clienteControl.valueChanges.subscribe(valor => {
+      this.clientesFiltrados = this.filtrarClientes(valor || '');
     });
-    this.Grupo = new FormGroup({
-      grupo: new FormControl(''),
-    })
   }
 
-  cadexer(){
-    if (this.exer.valid) {
-      // Obter os valores do formulário e converter para JSON
-      const dados = JSON.stringify(this.exer.getRawValue());
-      this.service.cadexer(dados).subscribe({
-        next:(dados) =>{
-          alert(dados.msg);
-          this.exer.reset();
-        }
-      })
-    }
+  pesquisarCli() {
+    this.cliservice.pesquisar().subscribe({
+      next: (dados) => {
+        this.clientes = dados;
+      },
+      error: (err) => {
+        console.log('ocorreu um erro: ' + err);
+      },
+    });
   }
 
-  cadgrupo(){
-    if (this.Grupo.valid) {
-      // Obter os valores do formulário e converter para JSON
-      const dados = JSON.stringify(this.Grupo.getRawValue());
-      this.service.cadgrupo(dados).subscribe({
-        next:(dados) =>{
-          alert(dados.msg);
-          this.Grupo.reset();
-        }
-      })
-    }
+  filtrarClientes(valor: string) {
+    const filtro = valor.toLowerCase();
+    return this.clientes.filter((c: { nome: string }) =>
+      c.nome.toLowerCase().includes(filtro)
+    );
+  }
+
+  selecionarCliente(nome: string) {
+    this.clienteSelecionado = this.clientes.find(
+      (c: { nome: string }) => c.nome === nome
+    );
+    console.log('Cliente selecionado:', this.clienteSelecionado);
   }
 }

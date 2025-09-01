9647c507c967ebb9e692dea723c33ffc423d6117
brenohenriquerr <breno.reis@aluno.ifsp.edu.br>
2025-09-01 17:57:03 -0300

    planos edit funcionando

--- Front-end/src/app/components/modais/modal-editar-planos/modal-editar-planos.component.ts
+++ Front-end/src/app/components/modais/modal-editar-planos/modal-editar-planos.component.ts
@@ -3,30 +3,40 @@ import { Component, input } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ConfigService } from '../../../services/admin/config/config.service';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ModalSpinnerComponent } from '../modal-spinner/modal-spinner.component';
+import { PlanosServiceService } from '../../../services/planosService/planos-service.service';
 
 @Component({
   selector: 'app-modal-editar-planos',
   standalone: true,
-  imports: [NgIf, ReactiveFormsModule],
+  imports: [NgIf, ReactiveFormsModule,ModalSpinnerComponent],
   templateUrl: './modal-editar-planos.component.html',
   styleUrl: './modal-editar-planos.component.css'
 })
 export class ModalEditarPlanosComponent {
   identificador: number = 0;
   tipo: string = '';
+  loading = false;
   planos: any;
   planosForm: FormGroup = new FormGroup({});;
-  novoBeneficio: string = ''; // Benefício atual que será adicionado
-  beneficioAtual: string = '';  // Para armazenar o benefício que está sendo adicionado ou editado
-  beneficios: string[] = [];    // Array para armazenar os benefícios
-  indiceEdicao: number | null = null; // Armazena o índice do benefício que está sendo editado
-  editar :boolean = false
+  // novoBeneficio: string = ''; // Benefício atual que será adicionado
+  // beneficioAtual: string = '';  // Para armazenar o benefício que está sendo adicionado ou editado
+  // beneficios: string[] = [];    // Array para armazenar os benefícios
+  // indiceEdicao: number | null = null; // Armazena o índice do benefício que está sendo editado
+  editar :boolean = false;
+  option_frequencia = [2,3,7];
 
-  ngOnInit(){   
-    this.buscarDados();
+  ngOnInit(){  
+    this.loading = true;
+    setTimeout(async () => {
+      await this.buscarDados();
+      this.loading = false;
+    },200);
+    
   }
 
-  constructor(private route: ActivatedRoute, private service: ConfigService,private fb: FormBuilder){
+  constructor(private route: ActivatedRoute, private service: ConfigService,
+    private fb: FormBuilder, private planosService: PlanosServiceService) {
     this.route.queryParams.subscribe(params => {
       this.identificador = params['id'];
       this.tipo = params['tipo'];
@@ -41,16 +51,18 @@ export class ModalEditarPlanosComponent {
       // beneficios: new FormControl (this.planos[0].beneficios, [Validators.required]),
       descricao: new FormControl (this.planos[0].descricao, [Validators.required]),
       duracao: new FormControl (this.planos[0].duracao, [Validators.required]),
-      disponibilidade: new FormControl (this.planos[0].disponibilidade, [Validators.required])
+      disponibilidade: new FormControl (this.planos[0].disponibilidade, [Validators.required]),
+      dias_por_semana: new FormControl (this.planos[0].dias_por_semana, [Validators.required])
     });
     //  this.beneficios = this.planos[0].beneficios;
     //  console.log(this.beneficios);
-    this.beneficios = this.planosForm.get('beneficios')?.value
-      ? this.planosForm.get('beneficios')?.value.split(', ')
-      : [];
+    // this.beneficios = this.planosForm.get('beneficios')?.value
+    //   ? this.planosForm.get('beneficios')?.value.split(', ')
+    //   : [];
   }
 
-  buscarDados(){
+  buscarDados() : Promise<void>{
+     return new Promise((resolve, reject) => {
     switch (this.tipo) {
       case 'plano':
         this.service.pesquisarPlanosID(this.identificador).subscribe({
@@ -58,8 +70,10 @@ export class ModalEditarPlanosComponent {
             console.log(dados)
             this.planos = Array(dados);
             this.form();
+            resolve();
           },error: (erro) => {
             alert("erro ao pesquisar dados \n status:" + erro.status);
+            reject(erro);
           }
         })
         break;
@@ -69,48 +83,28 @@ export class ModalEditarPlanosComponent {
       default:
         break;
     }
+    });
   }
 
-  // adicionarOuEditarBeneficio() {
-  //   if (this.beneficioAtual.trim()) {
-  //     if (this.indiceEdicao === null) {
-  //       // Adicionando um novo benefício
-  //       this.beneficios.push(this.beneficioAtual.trim());
-  //     } else {
-  //       // Editando um benefício existente
-  //       this.beneficios[this.indiceEdicao] = this.beneficioAtual.trim();
-  //       this.indiceEdicao = null; // Resetar o índice de edição
-  //     }
-
-  //     this.planosForm.get('beneficios')?.setValue(this.beneficios.join(', '));
-      
-  //     // Limpa o campo de texto e atualiza o formulário
-  //     this.beneficioAtual = '';
-  //     this.editar = false;
-  //   }
-  // }
-
-  // editarBeneficio(index: number) {
-  //   this.editar = true;
-  //   this.beneficioAtual = this.beneficios[index]; // Carrega o benefício no textarea
-  //   this.indiceEdicao = index; // Define o índice do benefício que está sendo editado
-  // }
-
-  // removerBeneficio(index: number) {
-  //   this.beneficios.splice(index, 1); // Remove o benefício da lista
-  //   this.planosForm.get('beneficios')?.setValue(this.beneficios.join(', ')); // Atualiza o campo do formulário
-  //   console.log(this.planosForm.getRawValue());
-  //   this.beneficioAtual = '';
-  // }
-
-  // adicionarBeneficio() {
-  //   if (this.planosForm.value.beneficios.trim()) {
-  //     this.beneficios.push(this.planosForm.value.beneficios.trim());
-  //     this.planosForm.value.beneficios = ''; // Limpa o campo textarea
-
-  //     // Atualiza o campo 'beneficios' do formulário com todos os benefícios concatenados
-  //     this.planosForm.get('beneficios')?.setValue(this.beneficios.join(', '));
-  //   }
-  // }
+  salvar() {
+    if (this.planosForm.valid) {
+      this.loading = true;
+      const dadosAtualizados = JSON.stringify({...this.planosForm.value,id: this.identificador});
+      console.log(dadosAtualizados);
+      this.planosService.editar(dadosAtualizados).subscribe({
+        next: (resposta) => {
+          alert('Plano atualizado com sucesso!');
+          this.loading = false;
+          // Aqui você pode fechar o modal ou atualizar a tela conforme necessário
+        },
+        error: (erro) => {
+          alert('Erro ao atualizar plano\nStatus: ' + erro.status);
+          this.loading = false;
+        }
+      });
+    } else {
+      alert('Preencha todos os campos obrigatórios corretamente.');
+    }
+  }
 
 }

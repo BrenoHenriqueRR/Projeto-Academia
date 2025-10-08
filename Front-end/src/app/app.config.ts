1eacccbed5c2551ea7c36a4f8f43a261672e9798
brenohenriquerr <breno.reis@aluno.ifsp.edu.br>
2025-10-08 18:52:06 -0300

    calendario certo

--- Front-end/src/app/app.config.ts
+++ Front-end/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, LOCALE_ID } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { DatePipe, registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
@@ -9,7 +9,7 @@ import { provideToastr } from 'ngx-toastr';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideNgxMask } from 'ngx-mask';
-import { provideDaterangepickerLocale } from 'ngx-daterangepicker-bootstrap'; // ✅ Import do DateRangePicker
+import { NgxDaterangepickerMd } from 'ngx-daterangepicker-material';
 
 // registra o locale pt-BR do Angular
 registerLocaleData(localePt);
@@ -22,28 +22,11 @@ export const appConfig: ApplicationConfig = {
     provideAnimations(),
     provideHttpClient(withFetch()),
     provideAnimationsAsync(),
+     importProvidersFrom(NgxDaterangepickerMd.forRoot()),
     DatePipe,
 
     // ✅ Locale global da aplicação
     { provide: LOCALE_ID, useValue: 'pt-BR' },
 
-    // ✅ Configuração do ngx-daterangepicker-bootstrap
-    provideDaterangepickerLocale({
-      format: 'DD/MM/YYYY',
-      displayFormat: 'DD/MM/YYYY',
-      direction: 'ltr',
-      weekLabel: 'S',
-      separator: ' - ',
-      applyLabel: 'Aplicar',
-      cancelLabel: 'Cancelar',
-      clearLabel: 'Limpar',
-      customRangeLabel: 'Personalizado',
-      daysOfWeek: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
-      monthNames: [
-        'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
-        'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
-      ],
-      firstDay: 0 // domingo
-    })
   ],
 };
